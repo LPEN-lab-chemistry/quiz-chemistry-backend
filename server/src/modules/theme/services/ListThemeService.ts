556a891c5f9d8111a43d360ac1107d79657bb170
BrunoCiprianoDev <cipriano990@gmail.com>
2023-09-16 19:13:55 -0300

    feat(themes): Add pagination theme service

--- server/src/modules/theme/services/ListThemeService.ts
+++ server/src/modules/theme/services/ListThemeService.ts
@@ -1,10 +1,20 @@
 import prismaClient from '../../../prismaClient';
 interface IRequest {
   queryName: string;
+  page: number;
+  size: number;
 }
 
 class ListThemeService {
-  async execute({ queryName }: IRequest) {
+  async execute({ queryName, page, size }: IRequest) {
+    if (page < 1) {
+      page = 1;
+    }
+    if (size < 1) {
+      size = 10;
+    }
+
+    const skipAmount = (page - 1) * size;
     const listResponse = await prismaClient.theme.findMany({
       where: {
         name: {
@@ -12,6 +22,8 @@ class ListThemeService {
         },
         isDeleted: false,
       },
+      skip: skipAmount,
+      take: size,
       select: {
         id: true,
         name: true,
